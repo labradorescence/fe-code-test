7c62b37ceee0c3e7a8bbaaef91c45f6f53d5c7b8
Leigh <lsteiner@locus.co>
2019-11-14 15:12:30 -0500

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { mount, configure, shallow } from "enzyme";
+import { render } from "@testing-library/react";
+
+import CustomNav from "./CustomNav";
+import TopCardList from "./TopCardList";
+import LowerResultList from "./LowerResultList";
+import Search from './Search'
 import App from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+
+/*
+	Normally, I'd want to add more thorough tests, 
+	but given the time constraints, I focused on 
+	getting the essential minimum of coverage with a suite of 
+	snapshot tests to keep track of changes
+*/
+
+describe("snapshot suite", () => {
+  it("CustomNav looks right", () => {
+    const { container } = render(<CustomNav />);
+    expect(container).toMatchSnapshot();
+  });
+   it("TopCardList looks right", () => {
+    const { container } = render(<TopCardList articles={[]}/>);
+    expect(container).toMatchSnapshot();
+  });
+  it("LowerResultList looks right", () => {
+    const { container } = render(<LowerResultList  articles={[]} />);
+    expect(container).toMatchSnapshot();
+  });
+  it("Search looks right", () => {
+    const { container } = render(<Search />);
+    expect(container).toMatchSnapshot();
+  });
 });
--- src/Search.js
+++ src/Search.js
@@ -96,80 +96,10 @@ class SearchResults extends React.Component {
     );
   }
 }
-// const LowerResultList = props => {
-//   return (
-//     <span>
-//       {props.articles.length ? (
-//         <span className="lowerResultList__title">MOST POPULAR</span>
-//       ) : null}
-//       <ul className="lowerResultList">
-//         {props.articles.map((article, ind) => (
-//           <li className="searchCard searchCard--rest" key={ind}>
-//             <a className="searchCard--restLink" href={article.urlFull}>
-//               {article.ledeImage ? (
-//                 <img
-//                   className="lowerResult__image"
-//                   src={article.ledeImage.src}
-//                   alt={
-//                     article.ledeAltImage || "this needs a descriptive alt tag!"
-//                   }
-//                 />
-//               ) : (
-//                 <img
-//                   alt="filler kitten"
-//                   className="lowerResult__image"
-//                   src="http://placekitten.com/g/200/200"
-//                 />
-//               )}
-//               <span className="lowerResult__text">
-//                 <span
-//                   className="lowerResult__title"
-//                   dangerouslySetInnerHTML={{
-//                     __html: DOMPurify.sanitize(article.title)
-//                   }}
-//                 />
-//                 <span className="lowerResult__author">
-//                   {article.authors.map((author, ind) =>
-//                     ind < article.authors.length - 1
-//                       ? author.name + ", "
-//                       : author.name
-//                   )}
-//                 </span>
-//               </span>
-//             </a>
-//           </li>
-//         ))}
-//       </ul>
-//     </span>
-//   );
-// };
 
 SearchResults.propTypes = {
   term: PropTypes.string,
   limit: PropTypes.number
 };
-
-// LowerResultList.propTypes = {
-//   articles: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       nid: PropTypes.number,
-//       title: PropTypes.string,
-//       slug: PropTypes.string,
-//       publishedAt: PropTypes.string,
-//       url: PropTypes.string,
-//       authors: PropTypes.arrayOf(
-//         PropTypes.shape({
-//           name: PropTypes.string,
-//           slug: PropTypes.string
-//         })
-//       ),
-//       ledeImage: PropTypes.shape({
-//         src: PropTypes.string,
-//         width: PropTypes.number,
-//         height: PropTypes.number
-//       })
-//     })
-//   )
-// };
 
 export default Search;
